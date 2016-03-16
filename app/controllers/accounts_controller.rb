945f213bae5412d1f5eddd33d2e0f5f2fb8b5483
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-03-16 12:57:59 +0200

    some basic acc info

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,11 @@
 class AccountsController < ApplicationController
+  def index
+    @news_cs = Account.get_news_for_app(730)
+    @news_dota = Account.get_news_for_app(570)
+  end
+
+  def profile
+    @profile = Account.get_player_summary('76561197960435530')
+    @profile['personastate'] = Account.get_persona_state(@profile['personastate'])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :tests
+  resources :accounts do
+    get 'profile', on: :collection #member
+  end
 end
