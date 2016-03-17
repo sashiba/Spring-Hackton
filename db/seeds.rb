32576b1db44a624a7465913d48eb6a6105356ea6
Stefan Petrov <stefanp227@gmail.com>
2016-03-17 18:27:23 +0200

    Seeds test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  Weapon.create(name: 'ak47', picture: 'http://vignette2.wikia.nocookie.net/cswikia/images/7/79/Ak47hud_csgo.png/revision/latest?cb=20150530195803')
+  Weapon.create(name: 'm4a1s', picture: 'http://vignette2.wikia.nocookie.net/cswikia/images/3/39/M4a1-s.png/revision/latest?cb=20130824215315')
